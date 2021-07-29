cb6a1d5ca188bfb49ecc06f3cb74953bf9355481
maribernardes <maribernardes@gmail.com>
2021-07-29 14:17:58 -0300

    implemented needle_pose msg from matlab file

--- trajcontrol/virtual_robot.py
+++ trajcontrol/virtual_robot.py
@@ -1,9 +1,12 @@
 import rclpy
+import ament_index_python 
 
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Quaternion
 from transforms3d.euler import euler2quat
+from scipy.io import loadmat
+
 
 class VirtualRobot(Node):
 
@@ -22,6 +25,12 @@ class VirtualRobot(Node):
         self.publisher_needle = self.create_publisher(PoseStamped, '/needle/state/pose', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_needle_callback)
+
+        package_path = str(ament_index_python.get_package_share_path('trajcontrol'))
+        file_path = package_path + '/../../../../files/virtual_26.mat'
+        trial_data = loadmat(file_path, mat_dtype=True)
+        
+        self.needle_pose = trial_data['needle_pose'][0]
 
         self.i = 0
 
@@ -43,13 +52,17 @@ class VirtualRobot(Node):
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = "stage"
 
-        msg.pose.position.z = 3.0 + self.i
-        q = euler2quat(0, 0.1745+0.1*self.i, 0, 'rzyx')
-        msg.pose.orientation = Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
+        Z = self.needle_pose[self.i]
+        msg.pose.position.x = float(Z[0])
+        msg.pose.position.y = float(Z[1])
+        msg.pose.position.z = float(Z[2])
+
+        msg.pose.orientation = Quaternion(x=float(Z[3]), y=float(Z[4]), z=float(Z[5]), w=float(Z[6]))
 
         self.publisher_needle_pose.publish(msg)
-        self.get_logger().info('Publish - Needle pose: y=%f, q=[%f, %f, %f, %f] in %s frame'  % (msg.pose.position.y, msg.pose.orientation.x, \
-             msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w, msg.header.frame_id))
+        self.get_logger().info('Publish - Needle pose: x=%f, y=%f, z=%f, q=[%f, %f, %f, %f] in %s frame'  % (msg.pose.position.x, \
+            msg.pose.position.y, msg.pose.position.z,  msg.pose.orientation.x, \
+            msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w, msg.header.frame_id))
         self.i += 1
 
     def timer_needle_callback(self):
@@ -62,7 +75,7 @@ class VirtualRobot(Node):
         msg.pose.orientation = Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])
 
         self.publisher_needle.publish(msg)
-        self.get_logger().info('Publish - Needle pose: y=%f, q=[%f, %f, %f, %f] in %s frame'  % (msg.pose.position.y, msg.pose.orientation.x, \
+        self.get_logger().info('Publish - Needle: y=%f, q=[%f, %f, %f, %f] in %s frame'  % (msg.pose.position.y, msg.pose.orientation.x, \
              msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w, msg.header.frame_id))
         self.i += 1
 
