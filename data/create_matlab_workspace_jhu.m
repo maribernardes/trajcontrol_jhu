3908dd3c450d600914aa767d9a1b4234818c6da7
maribernardes <maribernardes@gmail.com>
2023-08-10 12:55:01 -0400

    Included matlab script to load demo data

--- data/create_matlab_workspace_jhu.m
+++ data/create_matlab_workspace_jhu.m
@@ -24,16 +24,22 @@ j = height(T);                 % Last sample
 sec = T{i:j,1};
 nanosec = T{i:j,2};
 
-% Aurora readings from igtl_bridge
-auroraTip = T{i:j,13:15};                 %Needle Tip (5DOF)
-auroraqTip = T{i:j,16:19};
+% Target and skin_entry [robot]
+target_r = T{i:j,3:5};
+skin_entry_r = T{i:j,8:10};
 
-% Published topics 
-% Tip = '/sensor/tip_filtered'
-% Base = '/stage/state/needle_pose'
-Tip = T{i:j,22:24};     %Needle Tip (5DOF)
-qTip = T{i:j,25:28};
-Base = T{i:j,40:42};    %[StageX DepthY StageZ]
+% Tip [needle] and [robot]
+posTip_n = T{i:j,13:15};
+qTip_n = T{i:j,16:19};
+posTip_r = T{i:j,22:24};
+qTip_r = T{i:j,25:28};
+
+% Base [needle] and [robot]
+posBase_n = T{i:j,31:33};       % [StageX -DepthY StageZ]
+qBase_n = T{i:j,34:37};         % Identity
+
+posBase_r = T{i:j,40:42};       % Base in needle frame (defined at initial experiment pose)
+qBase_r = T{i:j,43:46};         % Relative rotation between needle and robot frame
 
 % Jacobian (by lines)
 J1 = T{i:j,49:51};
@@ -42,22 +48,10 @@ J3 = T{i:j,55:57};
 J4 = T{i:j,58:60};
 J5 = T{i:j,61:63};
 
-%% Define variables to be saved in .mat
-% Total number of samples
-k = length(Tip);
-%Time vector
-t = zeros(1,k);
-% Aurora sensor values (in vector format [x, y, z, qw, qx, qy, qz])
-aurora_tip = zeros(7,k);
-% Sensor values in stage frame - published by ros2 topics (in vector format)
-tip = zeros(7,k);
-base = zeros(3,k);
-% Sensor values in stage frame - obtained with transform in matlab (in vector format)
-X = zeros(3,k);
-Z = zeros(5,k);
-% Jacobian matrix
-J = cell(1,k);
+depth = T{i:j,75};
+key = T{i:j,78};
 
+%% Save in matlab
 % Control outputs
 cmd = T{i:j,66:68}';
 % Target
@@ -67,7 +61,25 @@ base_init = T{i,40:42}';
 % Stage
 stage = 1000*T{i:j,71:72}';
 
-%% Iterate FIX THIS
+%% Define variables to be saved in .mat
+% Total number of samples
+k = length(sec);
+%Time vector
+t = zeros(1,k);
+% Aurora sensor values (in vector format [x, y, z, qw, qx, qy, qz])
+tip_n = zeros(7,k);
+base_n = zeros(3,k);
+% Sensor values in stage frame - published by ros2 topics (in vector format)
+tip_r = zeros(7,k);
+base_r = zeros(3,k);
+% Sensor values in stage frame - obtained with transform in matlab (in vector format)
+X = zeros(3,k);
+Z = zeros(5,k);
+% Jacobian matrix
+J = cell(1,k);
+
+
+%% Separate when controller was ON
 [~, initcmd] = find(cmd, 1,'first');
 for i=1:initcmd-1
     cmd(:,i) = base_init; 
@@ -77,19 +89,20 @@ for i=1:k
     %Set time vector in seconds
     t(i) = sec(i)-sec(1) + (nanosec(i)-nanosec(1))*1e-9;
     
-    aurora_tip(:,i) = [auroraTip(i,:) auroraqTip(i,:)]';
-    tip(:,i) = [Tip(i,:) qTip(i,:)]';
-    base(:,i) = Base(i,1:3)';
+    tip_n(:,i) = [posTip_n(i,:) qTip_n(i,:)]';
+    tip_r(:,i) = [posTip_r(i,:) qTip_r(i,:)]';
+    base_n(:,i) = posBase_n(i,1:3)';
+    base_r(:,i) = posBase_r(i,1:3)';
     
-    [horiz, vert]= get_needle_angles(qTip(i,:));
-    Z(:,i) = [Tip(i,:), horiz, vert];
-    X(:,i) = Base(i,1:3)';
+    [horiz, vert]= get_needle_angles(qTip_r(i,:));
+    Z(:,i) = [posTip_r(i,:), horiz, vert];
+    X(:,i) = posBase_r(i,1:3)';
     
     J{i} = [J1(i,:); J2(i,:); J3(i,:); J4(i,:); J5(i,:)];
 end
 
 % Indexes with key being hit
-key = [-5; diff(base(2,:))']/-5;
+key_index = find(key);
 
 % Load predictions from mat file
 matfile_name = strcat(folder,'/',name,num2str(trial,'%2.2d'),extra,'_pred.mat');
@@ -118,9 +131,9 @@ if isfile(matfile_name)
         end
 
     end
-    save(strcat('Matlab/',folder,'/',name,num2str(trial,'%2.2d'),extra,'.mat'), 't','aurora_tip', 'tip','base', 'X', 'Z', 'J', 'cmd', 'target', 'base_init', 'stage', 'key', 'up', 'yp');
+    save(strcat('Matlab/',folder,'/',name,num2str(trial,'%2.2d'),extra,'.mat'), 't','tip_n', 'tip_r','base_n', 'base_r', 'X', 'Z', 'J', 'cmd', 'target', 'base_init', 'stage', 'depth', 'key', 'up', 'yp');
 else    
-    save(strcat('Matlab/',folder,'/',name,num2str(trial,'%2.2d'),extra,'.mat'), 't','aurora_tip', 'tip','base', 'X', 'Z', 'J', 'cmd', 'target', 'base_init', 'stage', 'key');
+    save(strcat('Matlab/',folder,'/',name,num2str(trial,'%2.2d'),extra,'.mat'), 't','tip_n', 'tip_r','base_n', 'base_r', 'X', 'Z', 'J', 'cmd', 'target', 'base_init', 'stage', 'depth', 'key', 'key_index');
 end
 
 function [horizontal, vertical]= get_needle_angles(quat)
