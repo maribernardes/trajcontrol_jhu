49224119b9253f39e9470c43c566be278201b3ba
maribernardes <maribernardes@gmail.com>
2023-11-21 11:20:25 -0500

    Included sim_level option for launching virtual template

--- launch/manual_smart.launch.py
+++ launch/manual_smart.launch.py
@@ -1,55 +1,74 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
+
+from launch import LaunchDescription, conditions
+from launch.substitutions.launch_configuration import LaunchConfiguration
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.substitutions import PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
-from launch.actions import LogInfo
-
-import sys
-
-from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
-from launch import LaunchDescription, actions
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import ThisLaunchFileDir
 
-# Launch SmartTemplate in manual mode
-# Remember to launch keypress node (smart_template package) in another terminal
+# Launch SmartTemplate in manual control mode
+# Remember to run keypress node (trajcontrol package) in another terminal
 
 def generate_launch_description():
 
-    # robot = Node(
-    #     package="smart_template",
-    #     executable="template",
-    # )  
+    ld = LaunchDescription()
 
-    robot = Node(
-        package="smart_template",
-        executable="virtual_template",
+    arg_sim_level = DeclareLaunchArgument(
+        "sim_level",
+        default_value = "2",
+        description = "virtual = 1, real = 2"
     )  
-
-    manual_control = Node(
-        package="trajcontrol",
-        executable = "controller_manual_smart",
-        parameters = [{"motion_step": LaunchConfiguration('motion_step')}]
-    )   
-
-    initialization = Node(
-        package="smart_template",
-        executable = "initialization"
-    )   
-
-    return LaunchDescription([
-        DeclareLaunchArgument(
+    arg_motion_step = DeclareLaunchArgument(
             "motion_step",
             default_value = "1.0",
             description = "Step size in mm"
-        ),
-        actions.LogInfo(msg = ["motion_step: ", LaunchConfiguration('motion_step')]),
-        robot, 
-        initialization,
-        manual_control
-    ])
+    )
+
+    launch_directory = os.path.join(get_package_share_directory('smart_template'), 'launch')
+    hardware_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_directory, 'robot.launch.py')),
+        condition=conditions.IfCondition(
+            PythonExpression([LaunchConfiguration('sim_level'), " == 2"])
+        )
+    )
+    virtual_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(launch_directory, 'virtual_robot.launch.py')),
+        condition=conditions.IfCondition(
+            PythonExpression([LaunchConfiguration('sim_level'), " == 1"])
+        )
+    )
+
+    # Use keyboard interface
+    initialization = Node(
+        package = "smart_template",
+        executable = "initialization",
+    )
+
+    # Use manual controller
+    manual_control = Node(
+        package = "trajcontrol",
+        executable = "controller_manual_smart",
+        parameters = [
+            {"motion_step": 1.0}
+            ]
+    )
+
+    # # Save data to filename defined by user
+    # save_file = Node(
+    #     package = "trajcontrol",
+    #     executable = "save_file",
+    #     parameters =[{"filename": LaunchConfiguration('filename')}]
+    # )
+
+    # Include launch arguments
+    ld.add_action(arg_sim_level)
+    ld.add_action(arg_motion_step)
+    
+    ld.add_action(hardware_launch)
+    ld.add_action(virtual_launch)
+    ld.add_action(initialization)
+    ld.add_action(manual_control)
+    
+    return ld
