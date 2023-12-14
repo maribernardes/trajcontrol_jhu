Updated angle calculation with corrected shape_sensing

trajcontrol/trajcontrol/smart_needle_interface.py
@@ -69,9 +69,10 @@ class SmartNeedleInterface(Node):
         self.skin_entry = np.empty(shape=[0,3])         # Skin entry at begining of experiment (robot frame)
         self.air_gap_needle = np.empty(shape=[0,3])     # Distance between needle guide and skin entry at begining of experiment (needle frame)
         
-        self.stage = np.empty(shape=[0,3])              # Robot current position (robot frame)
-        self.needle_pose = np.empty(shape=[0,7])        # Base pose (needle frame)
-        self.tip = np.empty(shape=[0,7])                  # Tip pose (robot frame)
+        self.stage = np.empty(shape=[0,3])              # Robot position (robot frame)
+        self.needle_pose = np.empty(shape=[0,7])        # Robot position (needle frame)
+        self.tip = np.empty(shape=[0,7])                # Tip pose (robot frame)
+        self.tip_needle = np.empty(shape=[0,7])         # Tip pose (needle frame)
 
         # NeedleShape Bridge message
         self.shapecount = 0                         # Number of shape packace received
@@ -129,7 +130,7 @@ class SmartNeedleInterface(Node):
     #### Service server ###################################################
 
         # Service server to return planning points
-        self.service_server_tip = None   # Activate only after self.tip is available
+        self.service_server_tip = None          # Activate only after self.tip is available
 
     #### Variables initialization ###################################################
 
@@ -196,6 +197,8 @@ class SmartNeedleInterface(Node):
             response.valid = False
         else:
             response.valid = True
+            self.get_logger().info('Tip p (needle) = [%f, %f, %f]' %(self.tip_needle[0], self.tip_needle[1], self.tip_needle[2]))
+            self.get_logger().info('Tip q (needle) = [%f, %f, %f, %f]' %(self.tip_needle[3], self.tip_needle[4], self.tip_needle[5], self.tip_needle[6]))
             response.x = self.tip[0]
             response.y = self.tip[1]
             response.z = self.tip[2]
@@ -230,19 +233,8 @@ class SmartNeedleInterface(Node):
         q_tip = np.array([shape[N-1].orientation.w, shape[N-1].orientation.x, shape[N-1].orientation.y, shape[N-1].orientation.z])
         # Transform from needle to robot frame
         if (self.needleToRobot.size != 0): 
-            self.get_logger().info('Tip q (needle) = %s' %q_tip)
-            ##############
-            # Quick fix to put tip orientation in needle frame
-            # TODO: Remove this fix when shape_sensing publishes correctly in needle frame
-            q_wrong = np.quaternion(q_tip[0], q_tip[1], q_tip[2], q_tip[3])
-            q_fix = np.quaternion(self.needleToRobot[3], self.needleToRobot[4], self.needleToRobot[5], self.needleToRobot[6])
-            q_correct = q_fix*q_wrong
-            q_tip = np.array([q_correct.w, q_correct.x, q_correct.y, q_correct.z])
-            ##############
-            self.get_logger().info('Tip q (needle corr) = %s' %q_tip)
-            tip_needle = np.array([p_tip[0], p_tip[1], p_tip[2], q_tip[0], q_tip[1], q_tip[2], q_tip[3]]) # needle tip in needle frame
-
-            self.tip = pose_transform(tip_needle, self.needleToRobot)             # needle tip in robot frametimer_skin_entry_needle_callback
+            self.tip_needle = np.array([p_tip[0], p_tip[1], p_tip[2], q_tip[0], q_tip[1], q_tip[2], q_tip[3]]) # needle tip in needle frame
+            self.tip = pose_transform(self.tip_needle, self.needleToRobot)             # needle tip in robot frametimer_skin_entry_needle_callback
             if self.service_server_tip is None:
                 self.service_server_tip = self.create_service(GetPose, '/needle/get_tip', self.get_tip_callback)
                 self.get_logger().info('/needle/get_tip service is available')

trajcontrol/trajcontrol/utils.py
@@ -3,6 +3,96 @@ import numpy as np
 import quaternion
 import math
 
+
+
+
+#             error = self.target - self.tip[0:3]
+#             # Print values
+#             self.get_logger().info('\n****** STEP #%i ******\nInsertion depth: %f\nTarget: (%f, %f, %f)\nTip: (%f, %f, %f)\
+#                 \nError: (%f, %f, %f / %f (%f deg), %f (%f deg))\nJ: %s\nStage: (%f, %f, %f)\nCmd: (%f, %f, %f)\nReached: (%.4f, %.4f, %.4f)\n*********************' \
+#                 %(self.step, self.depth, \
+#                 self.target[0], self.target[1], self.target[2],\
+#                 self.tip[0],self.tip[1], self.tip[2],\
+#                 error[0], error[1], error[2], self.tip[3], math.degrees(self.tip[3]), self.tip[4], math.degrees(self.tip[4]),\
+#                 self.Jc, \
+#                 self.stage[0], self.stage[1], self.stage[2],\
+#                 self.cmd[0], self.cmd[1], self.cmd[2], \
+#                 result.x, result.y, result.z)
+#             )
+
+
+# #Array of data
+# header = ['Timestamp sec', 'Timestamp nanosec', \
+#     'Step', \
+#     'Insertion_depth', \
+#     'Skin_entry_r x', 'Skin_entry_r y', 'Skin_entry_r z', \
+#     'Target_r x', 'Target_r y', 'Target_r z', \
+#     'Tip_n x', 'Tip_n y', 'Tip_n z', 'Tip_n qw', 'Tip_n qx', 'Tip_n qy', 'Tip_n qz' \
+#     'Tip_r x', 'Tip_r y', 'Tip_r z', 'Tip_r qw', 'Tip_r qx', 'Tip_r qy', 'Tip_r qz', 'Tip_r sec', 'Tip_r nanosec', \
+#     'Err x', 'Err y', 'Err z', 'Err h', 'Err v', \
+#     'Stage_r x', 'Stage_r y', 'Stage_r z', 'Stage_r qw', 'Stage_r qx', 'Stage_r qy', 'Stage_r qz', \
+#     'J00', 'J01', 'J02', \
+#     'J10', 'J11', 'J12', \
+#     'J20', 'J21', 'J22', \
+#     'J30', 'J31', 'J32', \
+#     'J40', 'J41', 'J42', \
+#     'Control x', 'Control y', 'Control z', \
+# ]
+        
+#         with open(self.filename, 'w', newline='', encoding='UTF8') as f: # open the file in the write mode
+#             writer = csv.writer(f)  # create the csv writer
+#             writer.writerow(header) # write a row to the csv file
+
+#         #Last data received
+#         self.target_r = [0,0,0, 0,0]             #target point + sec nanosec (sensor_processing - UI)
+#         self.skin_entry_r = [0,0,0, 0,0]        #skin entry point + sec nanosec (sensor_processing - UI)
+#         self.tip_n = [0,0,0,0,0,0,0, 0,0]     #tip from /needle/state/current_shape (x, y, z, qw, qx, qy, qz) + sec nanosec (shape_sensing)
+#         self.tip_r = [0,0,0,0,0,0,0, 0,0]        #/sensor/tip (x, y, z, qw, qx, qy, qz) + sec nanosec (sensor_processing)
+#         self.base_n = [0,0,0,0,0,0,0, 0,0] #/stage/state/needle_pose (x, y(depth), z, 1, 0, 0, 0) + sec nanosec (sensor_processing)
+#         self.base_r = [0,0,0,0,0,0,0, 0,0]       #/sensor/base (x, y(depth), z, 1, 0, 0, 0) + sec nanosec (sensor_processing)
+#         self.J = [0,0,0, \
+#                   0,0,0, \
+#                   0,0,0, \
+#                   0,0,0, \
+#                   0,0,0]            # /needle/state/jacobian Matrix (estimator)
+#         self.Jtime = [0,0]          # Jacobian sec nanosec
+#         self.cmd = [0,0,0, 0,0]     # /stage/control/cmd (x,y,z) + sec nanosec (sensor_processing)
+#         self.stage = [0,0, 0,0]     # stage/state/pose (x,z) + sec nanosec  (stage_control)
+#         self.depth = [0, 0,0]       # /arduino/depth (z) + sec nanosec  (depth_measurement)
+#         self.key = [0, 0,0]         # /keyboard/key (k) + sec nanosec  (keypress)
+#         self.get_logger().info('Log data will be saved at %s' %(self.filename))   
+
+
+
+# #Save data do file
+# def write_file_callback(filename, clock, target, skin_entry, tip, stage, J, cmd):
+#     now = self.get_clock().now().to_msg()
+    
+#     data = [now.sec, now.nanosec, \
+#         self.target_r[0], self.target_r[1], self.target_r[2], self.target_r[3], self.target_r[4],\
+#         self.skin_entry_r[0], self.skin_entry_r[1], self.skin_entry_r[2], self.skin_entry_r[3], self.skin_entry_r[4],\
+#         self.tip_n[0], self.tip_n[1], self.tip_n[2], self.tip_n[3], self.tip_n[4], self.tip_n[5], self.tip_n[6], self.tip_n[7], self.tip_n[8], \
+#         self.tip_r[0], self.tip_r[1], self.tip_r[2], self.tip_r[3], self.tip_r[4], self.tip_r[5], self.tip_r[6], self.tip_r[7], self.tip_r[8], \
+#         self.base_n[0], self.base_n[1], self.base_n[2], self.base_n[3], self.base_n[4], self.base_n[5], self.base_n[6], self.base_n[7], self.base_n[8],\
+#         self.base_r[0], self.base_r[1], self.base_r[2], self.base_r[3], self.base_r[4], self.base_r[5], self.base_r[6], self.base_r[7], self.base_r[8],\
+#         self.J[0], self.J[1], self.J[2], \
+#         self.J[3], self.J[4], self.J[5], \
+#         self.J[6], self.J[7], self.J[8], \
+#         self.J[9], self.J[10], self.J[11], \
+#         self.J[12], self.J[13], self.J[14], self.Jtime[0] , self.Jtime[1], \
+#         self.cmd[0], self.cmd[1], self.cmd[2], self.cmd[3], self.cmd[4], \
+#         self.stage[0], self.stage[1], self.stage[2], self.stage[3], \
+#         self.depth[0], self.depth[1], self.depth[2], \
+#         self.key[0], self.key[1], self.key[2], \
+#         ]
+#     # Reset keyboard
+#     self.key = [0, 0,0]
+
+#     with open(self.filename, 'a', newline='', encoding='UTF8') as f: # open the file in append mode
+#         writer = csv.writer(f) # create the csv writer
+#         writer.writerow(data)  # append a new row to the existing csv file    
+
+
 ########################################################################
 
 # Function: get_angles(q)
@@ -15,14 +105,10 @@ def get_angles(q):
 
     #Define rotation quaternion
     q_tf= np.quaternion(q[0], q[1], q[2], q[3])
-
-    #Get needle insertion axis (in needle frame)
-    #TODO: Fix orientation when shape_sensing is corrected
-    # u_dir = np.quaternion(0, 0, 0, 1) # When shape_sensing is corrected
-    u_dir = np.quaternion(0, 0, 1, 0) # For now
-    
+    #Set needle insertion axis (in needle frame)
+    u_dir = np.quaternion(0, 0, 0, 1) # Needle insertion in Z-axis (needle frame)
+    # Rotate direction vector with rotation quaternion
     v = q_tf*u_dir*q_tf.conj()
-
     #Angles are components in x (horizontal) and z(vertical)
     horiz = math.atan2(v.x, v.y)
     vert = math.atan2(v.z, math.sqrt(v.x**2+v.y**2))